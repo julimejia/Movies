7288e7a012a459a3fc07efd583c6b7312817df18
julimejia <davidmejiao43@gmail.com>
2023-08-30 23:04:16 -0500

    commit taller 2

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Movie
 
 
 
 def home (request):  
-   return render(request, 'home.html', {'name': 'julian mejia',})
+   searchTerm = request.GET.get('searchMovie')
+   if searchTerm:
+      mov = Movie.objects.filter(title__icontains=searchTerm)
+   else: 
+      mov = Movie.objects.all()
+   return render(request, 'home.html', {'movies': mov,})
 
 def about(request):  
    return render(request, 'about.html')
+
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from  movie import views
+from django.conf.urls.static import static
+from django.conf import settings
+from news import views as nv
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.home),
-    path('about/', views.about), 
+    path("", views.home, name = 'home_url'),
+    path('about/', views.about),
+    path('news/', include('news.urls'), name = 'news_url'), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
